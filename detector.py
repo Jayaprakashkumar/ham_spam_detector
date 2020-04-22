d55d755feb023a762c2f108983cb87bcc32000b9
Jayaprakashkumar <jayaprakash6034@gmail.com>
2020-04-21 23:09:19 -0400

    bags created

--- detector.py
+++ detector.py
@@ -1,6 +1,6 @@
 import os
 import collections
-
+import re
 
 def main():
     print("Hello")
@@ -12,29 +12,44 @@ def readFile():
     SpamDict = {}
     hamCounter =0
     spamCounter =0
-    your_path = '../data/train'
+    your_path = './data/train'
     files = os.listdir(your_path)
 
     for file in files:
-        print(file)
+        # print(file)
         if "ham" in file:
-            print("yes")
+            # print("yes")
             f=open(os.path.join(your_path,file),'r')
             for line in f:
-                for word in line.split():
-                    print(word)
-
-
+                line = re.sub('[^a-z\s]+',' ',line, flags=re.IGNORECASE)
+                line = re.sub('(\s+)',' ',line)
+                line = re.split(' ',line)
+                # print(line)
+                for word in line:
+                    if len(word) > 0:
+                        if word in HamDict:
+                            HamDict.update({word : HamDict[word] + 1})
+                        else:
+                            HamDict[word] = 1
+                         
             f.close()
-
+            print(HamDict)
 
         if "spam" in file:
-            print("yes")
+            # print("yes")
             f=open(os.path.join(your_path,file),'r')
             for line in f:
-                for word in line.split():
-                    print(word)
-
+                line = re.sub('[^a-z\s]+',' ',line, flags=re.IGNORECASE)
+                line = re.sub('(\s+)',' ',line)
+                line = re.split(' ',line)
+                # print(line)
+                for word in line:
+                    if len(word) > 0:
+                        if word in SpamDict:
+                            SpamDict.update({word : SpamDict[word] + 1})
+                        else:
+                            SpamDict[word] = 1
+                         
 
             f.close()
 
